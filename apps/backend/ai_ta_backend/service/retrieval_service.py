55dcf78ed76ce2a075004e248c91a9da4f03e243
rohan-uiuc <rohan13@illinois.edu>
2026-08-25 14:14:41 -0500

    Revert "fix: trunk check fix"
    This reverts commit 44aabb8c7c3914b54473cc2df58149f08334d7a8.

--- apps/backend/ai_ta_backend/service/retrieval_service.py
+++ apps/backend/ai_ta_backend/service/retrieval_service.py
@@ -25,6 +25,7 @@ from langchain.embeddings.ollama import OllamaEmbeddings
 # from langchain.chat_models import AzureChatOpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.schema import Document
+import openai
 import pytz
 from qdrant_client.http import models
 
